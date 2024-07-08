36a8c20655ed597694924188a1cdb27cef83c890
GoodboyBadvibes <eekwebene@gmail.com>
2024-07-08 23:04:41 +0100

    'deletedPagesDirectory'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,6 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
