7d2775801ee9f6d3a03afce04ca40d78918bebc9
GoodboyBadvibes <eekwebene@gmail.com>
2024-07-11 22:59:00 +0100

    extended installment to mobile view

--- src/Components/Dashboard/Dashbar.js
+++ src/Components/Dashboard/Dashbar.js
@@ -1,14 +1,13 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import style from '../../Style/Dashboard style/dashbar.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const Dashbar = () => {
 
-
-  const [navMenu,setNavMenu] = useState(false)
+  
 
   return (
     <>
@@ -45,7 +44,7 @@ const Dashbar = () => {
           </div>
          </div>
          
-            <i className={" ri-menu-fill float-end  sm:hidden cursor-pointer "+ (navMenu?' ':' ')} style={{fontSize:'25px'}} onClick={()=>{setNavMenu(true)}}></i>
+            <i className=" ri-menu-fill float-end  sm:hidden cursor-pointer " style={{fontSize:'25px', float:'inline-end'}} onClick={()=>{document.querySelector('#navMenu').classList.remove('hidden')}}></i>
         </div >
         
       </div>
@@ -61,59 +60,7 @@ const Dashbar = () => {
 
 
 
-      <div style={{backgroundColor:'rgb(36,36,36)', color:'white'}} className= {navMenu?"fixed sm:hidden w-full h-full":'hidden'}>
-        <div className=" flex justify-end">
-          <i className=" ri-close-fill cursor-pointer text-end items-end border-2 " style={{margin:'20px 25px', fontSize:'15px', borderRadius:'100%', padding:'0px 4px'}} onClick={()=>{setNavMenu(false)}}></i>
-        </div>
-
-        <Image className=' m-auto' src={'/logo.png'} width={150} height={150} priority={true}/>
-
-        <div>
-        <div  className=" text-center items-center w-fit  m-auto " style={{fontSize:'30px', marginTop:'50px'}}>
-        
-            <div className=' flex w-fit cursor-pointer hover:text-standard ' style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}} onClick={()=>{setComponent('summary')}}>
-              <i className='ri-line-chart-fill'></i>
-              {<h1>Summary</h1>}
-            </div>
-
-            <div className='flex w-fit cursor-pointer hover:text-standard' style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}}  onClick={()=>{setComponent('website')}}>
-              <i className='ri-flashlight-fill'></i>
-              {<h1>Website</h1>}
-            </div>
-
-            <div className='flex w-fit cursor-pointer hover:text-standard' style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}}  onClick={()=>{setComponent('messages')}}>
-              <i className='ri-discuss-fill'></i>
-              {<h1>Messages</h1>}
-            </div>
-
-            <div className='flex w-fit cursor-pointer hover:text-standard' style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}}  onClick={()=>{setComponent('products')}}>
-              <i className='ri-line-chart-fill'></i>
-              {<h1>Products</h1>}
-            </div>
-
-            <div className='flex w-fit cursor-pointer hover:text-standard' style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}}  onClick={()=>{setComponent('settings')}}>
-              <i className='ri-settings-2-fill'></i>
-              {<h1>Settings</h1>}
-            </div>
-          </div>
-
-          <div className=' flex justify-center items-center ' style={{gap:'10px', marginTop:'50px'}}>
-
-                <div className={' rounded-full  bg-standard '+style.dp} style={{ padding:'25px'}} >
-                  {/* <Image src={'/dp.png'} className=' ' style={{backgroundImage:' ', borderRadius:'100%',objectFit:'cover',objectPosition:'cover'}}  width={50} height={50} /> */}
-                </div>
-
-                <div>
-                  <h1 className='' style={{fontSize:'14px', fontWeight:'700',}}>Mighty Ness</h1>
-                  <p style={{fontSize:'10px', fontWeight:'400',}}>Mightyness@gmail.com</p>
-                </div>
-
-                <i className=' ri-arrow-down-wide-fill ' style={{color:'white', fontWeight:'700', cursor:'pointer'}}></i>
-
-              </div>
-        </div>
- 
-      </div>
+      
     </>
     
   )
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -40,27 +40,27 @@ const Dashboard = () => {
           </div>
 
           <div className='w-fit  m-auto '>
-            <div className=' flex w-fit cursor-pointer hover:text-standard ' style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}} onClick={()=>{setComponent('summary')}}>
+            <div className={' flex w-fit cursor-pointer hover:text-standard '+(component==="summary"?" text-standard":" ")} style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}} onClick={()=>{setComponent('summary')}}>
               <i className='ri-line-chart-fill'></i>
               {!expand &&<h1>Summary</h1>}
             </div>
 
-            <div className='flex w-fit cursor-pointer hover:text-standard' style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}}  onClick={()=>{setComponent('website')}}>
+            <div className={'flex w-fit cursor-pointer hover:text-standard '+(component==="website"?" text-standard":" ")} style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}}  onClick={()=>{setComponent('website')}}>
               <i className='ri-flashlight-fill'></i>
               {!expand &&<h1>Website</h1>}
             </div>
 
-            <div className='flex w-fit cursor-pointer hover:text-standard' style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}}  onClick={()=>{setComponent('transactions')}}>
+            <div className={'flex w-fit cursor-pointer hover:text-standard '+(component==="transactions"?" text-standard":" ")} style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}}  onClick={()=>{setComponent('transactions')}}>
               <i className='ri-discuss-fill'></i>
               {!expand &&<h1>Transactions</h1>}
             </div>
 
-            <div className='flex w-fit cursor-pointer hover:text-standard' style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}}  onClick={()=>{setComponent('products')}}>
+            <div className={'flex w-fit cursor-pointer hover:text-standard '+(component==="products"?" text-standard":" ")} style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}}  onClick={()=>{setComponent('products')}}>
               <i className='ri-line-chart-fill'></i>
               {!expand &&<h1>Products</h1>}
             </div>
 
-            <div className='flex w-fit cursor-pointer hover:text-standard' style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}}  onClick={()=>{setComponent('settings')}}>
+            <div className={'flex w-fit cursor-pointer hover:text-standard '+(component==="settings"?" text-standard":" ")} style={{ gap:'15px', fontSize:'14px',marginBottom:'16px'}}  onClick={()=>{setComponent('settings')}}>
               <i className='ri-settings-2-fill'></i>
               {!expand &&<h1>Settings</h1>}
             </div>
@@ -79,6 +79,63 @@ const Dashboard = () => {
       { component==='transactions'&& <Transactions expand={expand} />}
       { component==='products'&& <Products expand={expand} />}
       { component==='settings'&& <Settings expand={expand} />}
+
+
+     
+
+      <div style={{backgroundColor:'rgb(36,36,36)', color:'white'}} id='navMenu' className=' fixed top-0 sm:hidden w-full h-full hidden' >
+        <div className=" flex justify-end">
+          <i className=" ri-close-fill cursor-pointer text-end items-end border-2 " style={{margin:'20px 25px', fontSize:'15px', borderRadius:'100%', padding:'0px 4px'}} onClick={()=>{document.querySelector('#navMenu').classList.add('hidden')}}></i>
+        </div>
+
+        <Image className=' m-auto' src={'/logo.png'} width={150} height={150} priority={true}/>
+
+        <div>
+        <div  className=" text-center items-center w-fit  m-auto " style={{fontSize:'30px', marginTop:'50px'}}>
+        
+            <div className={' flex w-fit cursor-pointer hover:text-standard '+(component==="summary"?" text-standard":" ")} style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}} onClick={()=>{setComponent('summary'), document.querySelector('#navMenu').classList.add('hidden')}}>
+              <i className='ri-line-chart-fill'></i>
+              {<h1>Summary</h1>}
+            </div>
+
+            <div className={' flex w-fit cursor-pointer hover:text-standard '+(component==="website"?" text-standard":" ")} style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}}  onClick={()=>{setComponent('website'), document.querySelector('#navMenu').classList.add('hidden')}}>
+              <i className='ri-flashlight-fill'></i>
+              {<h1>Website</h1>}
+            </div>
+
+            <div className={' flex w-fit cursor-pointer hover:text-standard '+(component==="transactions"?" text-standard":" ")} style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}}  onClick={()=>{setComponent('transactions'), document.querySelector('#navMenu').classList.add('hidden')}}>
+              <i className='ri-discuss-fill'></i>
+              {<h1>Transactions</h1>}
+            </div>
+
+            <div className={' flex w-fit cursor-pointer hover:text-standard '+(component==="products"?" text-standard":" ")} style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}}  onClick={()=>{setComponent('products'), document.querySelector('#navMenu').classList.add('hidden')}}>
+              <i className='ri-line-chart-fill'></i>
+              {<h1>Products</h1>}
+            </div>
+
+            <div className={' flex w-fit cursor-pointer hover:text-standard '+(component==="settings"?" text-standard":" ")} style={{ gap:'15px', fontSize:'20px',marginBottom:'20px'}}  onClick={()=>{setComponent('settings'), document.querySelector('#navMenu').classList.add('hidden')}}>
+              <i className='ri-settings-2-fill'></i>
+              {<h1>Settings</h1>}
+            </div>
+          </div>
+
+          <div className=' flex justify-center items-center ' style={{gap:'10px', marginTop:'50px'}}>
+
+                <div className={' rounded-full  bg-standard '+style.dp} style={{ padding:'25px'}} >
+                  {/* <Image src={'/dp.png'} className=' ' style={{backgroundImage:' ', borderRadius:'100%',objectFit:'cover',objectPosition:'cover'}}  width={50} height={50} /> */}
+                </div>
+
+                <div>
+                  <h1 className='' style={{fontSize:'14px', fontWeight:'700',}}>Mighty Ness</h1>
+                  <p style={{fontSize:'10px', fontWeight:'400',}}>Mightyness@gmail.com</p>
+                </div>
+
+                <i className=' ri-arrow-down-wide-fill ' style={{color:'white', fontWeight:'700', cursor:'pointer'}}></i>
+
+              </div>
+        </div>
+ 
+      </div>
     </>
 
     
