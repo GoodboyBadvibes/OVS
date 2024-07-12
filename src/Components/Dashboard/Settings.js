a9a886566b48dd8e63d7bbc9aab8e5bdea5dbfb1
GoodboyBadvibes <eekwebene@gmail.com>
2024-07-12 22:18:29 +0100

    created settings bar

--- src/Components/Dashboard/Settings.js
+++ src/Components/Dashboard/Settings.js
@@ -1,10 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from '../../Style/Dashboard style/settings.module.css'
 
 const Settings = () => {
+
+  const [setting, setSetting] = useState('profile')
+
   return (
-    <div className='flex justify-center justify-items-center items-center h-screen'>
-      <h1>This page is still under development...</h1>
-    </div>
+    <>
+     <div className={style.component}>
+      {/* settings board section */}
+      <div className=' '  style={{backgroundColor:'white', borderRadius:'20px', maxWidth:'364px'}}>
+        <div className={style.settingItem} onClick={()=>{setSetting('profile')}}>
+          <div className={' flex justify-between items-center cursor-pointer '} style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)', paddingBottom:'19px'}}>
+            <div>
+              <h1 className={style.title +' '+(setting==='profile'?' ':style.notActive)} style={{}}>User Profile</h1>
+              <p className={style.info}>View Profile</p>
+            </div>
+            <i className= 'ri-arrow-right-wide-fill' ></i>
+          </div>
+        </div>
+        <div className={style.settingItem} onClick={()=>{setSetting('password')}}>
+          <div className={' flex justify-between items-center cursor-pointer '} style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)', paddingBottom:'19px'}}>
+            <div>
+              <h1 className={style.title +' '+(setting==='password'?' ':style.notActive)} style={{}}>Change Password</h1>
+              <p className={style.info}>Change account password</p>
+            </div>
+            <i className= 'ri-arrow-right-wide-fill' ></i>
+          </div>
+        </div>
+        <div className={style.settingItem} onClick={()=>{setSetting('notifications')}}>
+          <div className={' flex justify-between items-center cursor-pointer '} style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)', paddingBottom:'19px'}}>
+            <div>
+              <h1 className={style.title +' '+(setting==='notifications'?' ':style.notActive)} style={{}}>Notifications</h1>
+              <p className={style.info}>Notification options</p>
+            </div>
+            <i className= 'ri-arrow-right-wide-fill' ></i>
+          </div>
+        </div>
+        <div className={style.settingItem} onClick={()=>{setSetting('2FA')}}>
+          <div className={' flex justify-between items-center cursor-pointer '} style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)', paddingBottom:'19px'}}>
+            <div>
+              <h1 className={style.title +' '+(setting==='2FA'?' ':style.notActive)} style={{}}>Enable 2FA</h1>
+              <p className={style.info}>Secure account with Two-Factor Authentication</p>
+            </div>
+            <i className= 'ri-arrow-right-wide-fill' ></i>
+          </div>
+        </div>
+        <div className={style.settingItem} onClick={()=>{setSetting('languages')}}>
+          <div className={' flex justify-between items-center cursor-pointer '} style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)', paddingBottom:'19px'}}>
+            <div>
+              <h1 className={style.title +' '+(setting==='languages'?' ':style.notActive)} style={{}}>Language</h1>
+              <p className={style.info}>Set and add languages</p>
+            </div>
+            <i className= 'ri-arrow-right-wide-fill' ></i>
+          </div>
+        </div>
+        <div className={style.settingItem} onClick={()=>{setSetting('subscription')}}>
+          <div className={' flex justify-between items-center cursor-pointer '} style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)', paddingBottom:'19px'}}>
+            <div>
+              <h1 className={style.title +' '+(setting==='subscription'?' ':style.notActive)} style={{}}>Subscription</h1>
+              <p className={style.info}>Manage subscription</p>
+            </div>
+            <i className= 'ri-arrow-right-wide-fill' ></i>
+          </div>
+        </div>
+        <div className={style.settingItem} onClick={()=>{setSetting('payments')}}>
+          <div className={' flex justify-between items-center cursor-pointer '} style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)', paddingBottom:'19px'}}>
+            <div>
+              <h1 className={style.title +' '+(setting==='payments'?' ':style.notActive)} style={{}}>Payment</h1>
+              <p className={style.info}>Manage Payments</p>
+            </div>
+            <i className= 'ri-arrow-right-wide-fill' ></i>
+          </div>
+        </div>
+        <div className={style.settingItem} onClick={()=>{setSetting('logout')}}>
+          <div className={' flex justify-between items-center cursor-pointer '} style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)', paddingBottom:'19px'}}>
+            <div>
+              <h1 className={style.title +' '+(setting==='logout'?' ':style.notActive)} style={{}}>Sign out</h1>
+              <p className={style.info}>Logout of user's account</p>
+            </div>
+            <i className= 'ri-arrow-right-wide-fill' ></i>
+          </div>
+        </div>
+        
+
+      </div>
+
+      <div>
+
+      </div>
+
+     </div>
+    </>
   )
 }
 
