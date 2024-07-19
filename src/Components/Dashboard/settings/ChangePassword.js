87d010b31235258cd187b7c87bfca141c580a63c
GoodboyBadvibes <eekwebene@gmail.com>
2024-07-19 23:39:39 +0100

    okay, so i just created the input fields

--- src/Components/Dashboard/settings/ChangePassword.js
+++ src/Components/Dashboard/settings/ChangePassword.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import style from '../../../Style/Dashboard style/settings.module.css'
+import style from '../../../Style/Dashboard style/changepassword.module.css'
+import settingstyle from '../../../Style/Dashboard style/settings.module.css'
 
 const ChangePassword = () => {
   return (
     <>
     <div>
-      <h1 className={style.outputTitle}>Change Password</h1>
+        <div className=' ' style={{borderBottom:' 0.5px solid rgba(0, 0, 0, 0.5)'}}>
+          <h1 className={settingstyle.outputTitle}>Change Password</h1>
+        </div>
+
+        <form style={{}} className= {style.form} action="">
+
+          <div className=' flex justify-start items-center gap-0'>
+            <input type="password" placeholder='Old password' /> 
+            <input type="text" placeholder='Show' disabled />
+          </div> 
+
+          <div className=' flex justify-start items-center gap-0'>
+            <input type="password" placeholder='New Password' /> 
+            <input type="text" placeholder='Show' disabled />
+          </div> 
+
+          <div className=' flex justify-start items-center gap-0'>
+            <input type="password" placeholder='Confirm Password' /> 
+            <input type="text" placeholder='Show' disabled />
+          </div> 
+
+        </form>
 
     </div>
     
