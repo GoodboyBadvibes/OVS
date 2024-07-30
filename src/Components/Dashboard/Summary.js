c8bb68eecf3e872745b6c48200f6d07a5cbdec4c
GoodboyBadvibes <eekwebene@gmail.com>
2024-07-30 20:31:12 +0100

    converted all cards to a single component

--- src/Components/Dashboard/Summary.js
+++ src/Components/Dashboard/Summary.js
@@ -3,6 +3,7 @@ import style from '../../Style/Dashboard style/summary.module.css'
 import navStyle from '../../Style/Dashboard style/dashbar.module.css'
 import Image from 'next/image'
 import Table from './Table'
+import Card from './Card'
 
 const Summary = ({expand}) => {
   return (
@@ -13,36 +14,11 @@ const Summary = ({expand}) => {
         <h4 style={{fontSize:'16px', fontWeight:'500', lineHeight:'18.93px '}}> Here's an overview of your activities</h4>
 
         <div className={' sm:grid grid-flow-col grid-cols-3 '+style.figuresGrid}>
-          <div className={' flex justify-start items-center '+style.figuresCard } style={{ backgroundColor:'#859f2793', borderRadius:'20px'}}>
-            <div className={style.figuresImage} style={{ backgroundColor:'#859f27', padding:'10px', borderRadius:'17.67px'}}>
-              <Image className=''  src={'/profile-2user 2.png'} width={25} height={25} />
-            </div>
           
-            <div>
-              <h1 className={style.figures}>1,200</h1>
-              <p className={style.figuresTitle}>Customers</p>
-            </div>
-         </div>
-          <div className={' flex justify-start items-center '+style.figuresCard } style={{ backgroundColor:'#859f2793', borderRadius:'20px'}}>
-            <div style={{ backgroundColor:'#859f27', padding:'10px', borderRadius:'17.67px'}}>
-              <Image className=''  src={'/clipboard-tick.png'} width={25} height={25} />
-            </div>
+          <Card src={'/profile-2user 2.png'} title={'Customers'} figure={'1,200'} />
+          <Card src={'/clipboard-tick.png'} title={'Transactions'} figure={'1,200'} />
+          <Card src={'/task-square.png'} title={'Earnings'} figure={'1,200'} />
           
-            <div>
-              <h1 className={style.figures}>1,200</h1>
-              <p className={style.figuresTitle}>Transactions</p>
-            </div>
-         </div>
-          <div className={' flex justify-start items-center '+style.figuresCard } style={{ backgroundColor:'#859f2793', borderRadius:'20px'}}>
-            <div style={{ backgroundColor:'#859f27', padding:'10px', borderRadius:'17.67px'}}>
-              <Image className=''  src={'/task-square.png'} width={25} height={25} />
-            </div>
-          
-            <div>
-              <h1 className={style.figures}>1,200</h1>
-              <p className={style.figuresTitle}>Earnings</p>
-            </div>
-         </div>
         </div>
 
         <div>
