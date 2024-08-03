dd1d952ac3e95a3c18d34d1c1dc305e1d8100d04
GoodboyBadvibes <eekwebene@gmail.com>
2024-08-03 09:48:21 +0100

    add product modal initiated

--- src/Components/Dashboard/Products.js
+++ src/Components/Dashboard/Products.js
@@ -1,12 +1,134 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
+import style from '../../Style/Dashboard style/product.module.css'
+import Table from './Table'
+import Image from 'next/image'
+import close from '../../../public/close-circle.png'
+import Button from '../Button'
 
-const Products = () => {
+const Products = ({}) => {
+
+  const [addPro, setAddPro] = useState(false)
+  const [addCat, setAddCat] = useState(false)
+
+  const data = [
+
+    {
+      ID:1,
+      post: 'Risk Management',
+      category:'Psychiatry',
+      customer:'John Doe',
+      status:'successful',
+      date:'12 Nov 2023',
+    },
+    {
+      ID:2,
+      post: 'Risk Management',
+      category:'Psychiatry',
+      customer:'John Doe',
+      status:'successful',
+      date:'12 Nov 2023',
+    },
+    {
+      ID:3,
+      post: 'Risk Management',
+      category:'Psychiatry',
+      customer:'John Doe',
+      status:'successful',
+      date:'12 Nov 2023',
+    },
+    {
+      ID:4,
+      post: 'Risk Management',
+      category:'Psychiatry',
+      customer:'John Doe',
+      status:'successful',
+      date:'12 Nov 2023',
+    },
+    {
+      ID:5,
+      post: 'Risk Management',
+      category:'Psychiatry',
+      customer:'John Doe',
+      status:'successful',
+      date:'12 Nov 2023',
+    },
+    {
+      ID:6,
+      post: 'Risk Management',
+      category:'Psychiatry',
+      customer:'John Doe',
+      status:'successful',
+      date:'12 Nov 2023',
+    },
+    {
+      ID:7,
+      post: 'Risk Management',
+      category: 'Psychiatry',
+      customer: 'John Doe',
+      date: '12 Nov 2023',
+      status: 'successful',
+    },
+   ]
+
   return (
-    <> 
-    <div>
-      <Card src={'/bag.png'} title={'Total Products'} figure={'10'} />
-    </div>
+    <>
+      <div className={style.products}>
+
+        <div className={' sm:flex justify-between items-center '}>
+          <Card src={'/bag.png'} title={'Total Products'} figure={'10'} style={{}} />
+
+          <div className=' sm:flex lg:gap-16 sm:gap-4'>
+            <div className={' '+ style.addBtn}>
+              <h1>Add a Category</h1>
+            </div>
+            <div className={' '+ style.addBtn}  onClick={()=>{setAddPro(true)}}>
+              <h1>Add a Product</h1>
+            </div>
+          </div>
+        </div>
+
+        <Table data={data} />
+      </div>
+
+      {addPro&&(<div className={' ' + style.modalDiv}>
+        <div className=' w-full h-full' style={{ display: 'flex', alignItems: ' center', justifyContent: 'center' }} >
+          <div className={' ' + style.modal}>
+            <div className=' flex justify-between items-center pb-4 md:pb-10 lg:pb-12 xl:pb-14'>
+              <h1 className={' '+style.modalTitle}>Add new Product</h1>
+              <Image src={close} style={{cursor:'pointer'}} onClick={()=>{setAddPro(false)}}/>
+            </div>
+            <form>
+              <h1 className={' text-standard ' + style.sub} style={{cursor:'pointer'}}>+ Upload product image</h1>
+              <div>
+                <h1 className={' py-7 ' + style.sub}>Product name</h1>
+                <input className={' '+style.input} type="text" name="" id="" placeholder='enter product name' />
+              </div>
+              <div className=' flex justify-between items-center gap-5'>
+                <div>
+                <h1 className={' py-7 ' + style.sub}>Units(available)</h1>
+                <input className={' '+style.input} type="text" />
+                </div>
+                <div>
+                <h1 className={' py-7 ' + style.sub}>SKU(optional)</h1>
+                <input className={' '+style.input} type="select" />
+                </div>
+                <div>
+                <h1 className={' py-7 ' + style.sub}>Category</h1>
+                <input className={' '+style.input} type="select" />
+                </div>
+              </div>
+              <div>
+              <h1 className={' py-7 ' + style.sub}>Product Description</h1>
+              <textarea className={' '+style.input} placeholder='write description' name="" id=""></textarea>
+              </div>
+              <Button text={'Add Product'} lass={style.btn}  style={{color:'white', width:'100%',textAlign:'center',fontSize:"16px", margin:"10px auto"}} />
+            </form>
+          </div>
+
+        </div>
+
+      </div>)}
     </>
   )
 }
