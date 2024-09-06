7750a1e2d3ce6884229dbaf75743bb2586e1d603
GoodboyBadvibes <eekwebene@gmail.com>
2024-09-06 20:02:21 +0100

    coming soon...

--- src/Components/Dashboard/Website.js
+++ src/Components/Dashboard/Website.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Website = () => {
   return (
     <div className='flex justify-center justify-items-center items-center h-screen'>
-      <h1>This page is still under development...</h1>
+      <h1 className=' text-5xl'>COMING SOON...</h1>
     </div>
   )
 }
